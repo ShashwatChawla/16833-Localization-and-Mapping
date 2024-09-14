4d7e0c6f944d52ca0637aaad3796980bea61b093
shashwat <shashwatchawla@gmail.com>
2024-09-14 19:30:09 -0400

    [ADD] Code for Low-Variance Resampling

--- HW1/hw1_problem_set/problem_set/code/resampling.py
+++ HW1/hw1_problem_set/problem_set/code/resampling.py
@@ -36,9 +36,43 @@ class Resampling:
         """
         TODO : Add your code here
         """
-        # X_bar_resampled =  np.zeros_like(X_bar)
+        # New Array of resampled wts
+        X_bar_resampled =  np.zeros_like(X_bar)
         
+        # Varibles for sampler
+        num_samples = X_bar.shape[0]
+        step        = 1/num_samples
+        u_rand      = np.random.uniform(0, step)
+        
+        if np.sum(X_bar[:, 3]) == 0:
+            X_bar_resampled = X_bar
+            print(f"[WARN]Returning X_bar as it is")
+            return X_bar_resampled
+        # Normalized Wt. arr
+        normalized_wts = X_bar[:, 3]/np.sum(X_bar[:, 3])
+        
+        # Cumalitive Wt. arr 
+        cumulative_wts = np.cumsum(normalized_wts)
+        
+        i = 0
+        # Sample Particles
+        for j in range(0, num_samples):
+            resampling_wt = u_rand + j*step
+            for c_idx, c_wt in enumerate(cumulative_wts):
+                if resampling_wt <= c_wt:
+                    X_bar_resampled[i, :3] = X_bar[c_idx, :3]
+                    break
+                
+                elif resampling_wt > c_wt and resampling_wt < cumulative_wts[c_idx+1]:
+                    X_bar_resampled[i, :3] = X_bar[c_idx+1, :3]
+                    break
+            
+            # print(f"Particle '{i}' replaced with '{c_idx}'")
+            i += 1
+        
+
         # For Testing Purposes
-        X_bar_resampled = X_bar 
+        # X_bar_resampled =  np.zeros_like(X_bar)
+        # X_bar_resampled = X_bar 
         
         return X_bar_resampled
