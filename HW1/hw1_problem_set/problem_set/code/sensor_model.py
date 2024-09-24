80f3114d0450bbb8fe649d6241f4070156603986
shashwat <shashwatchawla@gmail.com>
2024-09-24 18:50:41 -0400

    [BUG] p_max | [ADD] Vectorized beam model code w/t other funcs

--- HW1/hw1_problem_set/problem_set/code/sensor_model.py
+++ HW1/hw1_problem_set/problem_set/code/sensor_model.py
@@ -12,6 +12,53 @@ from scipy.stats import norm
 
 from map_reader import MapReader
 import pickle
+# from numba import njit, prange
+import multiprocessing as mp
+
+def worker_function(particle_data):
+    # Unwrapping particle data
+    x_t1_transformed_particle, expected_ray_vals, ang_arr = particle_data
+    
+    theta = x_t1_transformed_particle[2]
+
+    # Consider till just two decimals
+    laser_min = round(theta - np.pi/2, 2)
+    laser_max = round(theta + np.pi/2, 2)
+
+    # Wrap laser_min and laser_max if out of bounds
+    if laser_min < -np.pi:
+        laser_min += 2 * np.pi
+    if laser_max > np.pi:
+        laser_max -= 2 * np.pi
+
+    # Initialize the expected measurements for the particle
+    z_t1_arr_expected = np.zeros(180, dtype=np.float64)
+    if np.sum(expected_ray_vals) == 0:
+        # print(f"Cannot find obstacle")
+        return z_t1_arr_expected        
+
+    # Basic Condition
+    if laser_min <= laser_max:
+        closest_min_idx = np.argmin(np.abs(ang_arr - laser_min))
+        closest_max_idx = closest_min_idx + 180
+        z_t1_arr_expected = np.array(expected_ray_vals[closest_min_idx:closest_max_idx])
+    else:
+        closest_min_idx = np.argmin(np.abs(ang_arr - laser_min))
+        closest_max_idx = np.argmin(np.abs(ang_arr - laser_max))
+
+        # Combine values from two segments
+        first_segment = expected_ray_vals[closest_min_idx:]
+        second_segment = expected_ray_vals[:closest_max_idx]
+
+        #TODO: This should not be required. Debug
+        diff = 180 - len(first_segment) + len(second_segment) 
+        # Adjust to ensure the total length is exactly 180 readings  
+        if diff > 0:
+            second_segment = expected_ray_vals[:closest_max_idx + diff]
+        
+        z_t1_arr_expected = np.concatenate((first_segment, second_segment))[:180]
+        
+    return z_t1_arr_expected
 
 class SensorModel:
     """
@@ -29,9 +76,9 @@ class SensorModel:
         # self._z_max = 0.1
         # self._z_rand = 100
 
-        self._z_hit = 50
+        self._z_hit = 10
         self._z_short = 40
-        self._z_max = 10
+        self._z_max = 20
         self._z_rand = 50
         
         # self._z_hit = 0.05
@@ -70,6 +117,22 @@ class SensorModel:
         print("Sensor map loaded successfully.")
 
     
+
+
+    def vec_centre2laser_transform(self, x_t1_particles, offset=None):
+
+        if offset is None:
+            offset = self.laser_offset
+
+        # Compute the new x and y positions using vectorized operations
+        x_t1_l_x = x_t1_particles[:, 0] + offset * np.cos(x_t1_particles[:, 2])
+        x_t1_l_y = x_t1_particles[:, 1] + offset * np.sin(x_t1_particles[:, 2])
+
+        # Combine the new positions with the original orientations (theta)
+        x_t1_l_particles = np.column_stack((x_t1_l_x, x_t1_l_y, x_t1_particles[:, 2]))
+
+        return x_t1_l_particles
+
     # Function to Tranform from Centre to Laser offset
     def centre2laser_transform(self, x_t1, offset=None, visualize=True):
         
@@ -81,30 +144,8 @@ class SensorModel:
                            (x_t1[1] + offset*np.sin(x_t1[2])),
                             x_t1[2]], dtype=np.float64)
 
-
-        if visualize is True:
-            x_locs = x_t1[0] / 10.0
-            y_locs = x_t1[1] / 10.0
-            scat1 = plt.scatter(x_locs, y_locs, c='r', marker='o', s=10)
-
-            # Alternative arrow marker(w/t degree rot)
-            # arrow_length = 0.25
-            # arrow_width = 0.1
-            # dx = arrow_length * np.cos(x_t1[2])  # X-component of arrow direction
-            # dy = arrow_length * np.sin(x_t1[2])  # Y-component of arrow direction
-            # scat1 = plt.arrow(x_locs, y_locs, dx, dy, head_width=arrow_width, head_length=arrow_width, fc='red', ec='red')
-    
-            
-            x_locs_l = x_t1_l[0] / 10.0
-            y_locs_l = x_t1_l[1] / 10.0
-            scat2 = plt.scatter(x_locs_l, y_locs_l, c='g', marker='o', s=10)
-            
-            plt.pause(1)
-            scat1.remove()
-            scat2.remove()
             
         return x_t1_l 
-
 
     # Function to trace rays for all free locatns in map 
     def trace_map(self):
@@ -189,46 +230,33 @@ class SensorModel:
 
         return z_t1_arr_expected
                 
-
-    def ray_trace_measurements(self, x, y):
-        angle_values = self.particle_rays[x, y]
-        return angle_values 
-
-    def beam_range_finder_model(self, z_t1_arr, x_t1):
-        """
-        param[in] z_t1_arr : laser range readings [array of 180 values] at time t
-        param[in] x_t1 : particle state belief [x, y, theta] at time t [world_frame]
-        param[out] prob_zt1 : likelihood of a range scan zt1 at time t
-        """
-        """
-        TODO : Add your code here
-        """
+    def expected_ray_measurements(self, x_t1_transformed):
+        
+        # Conver to Occupany-map
+        map_x, map_y = int(x_t1_transformed[0]/self.map_resolution), int(x_t1_transformed[1]/self.map_resolution)
 
         z_t1_arr_expected = np.zeros((180), dtype=np.float64)
-        
-        # Centre to Laser Offset Transform
-        x_t1_transformed = self.centre2laser_transform(x_t1, visualize=False)
 
-        # Searhch Hash-map for Ray-Tracing
-        map_x, map_y = int(x_t1_transformed[0]/self.map_resolution), int(x_t1_transformed[1]/self.map_resolution)
+        # Search Ray-Tracing Hash-map 
         if (map_x, map_y) in self.particle_rays:
             exprected_ray_vals = self.particle_rays[(map_x, map_y)]
         else:
             # Probability zero since the particle is out of free-space(probably lol)
-            return 0
-            
-        ang_arr = np.arange(-3.14, 3.14, np.pi/180)
+            # print(f"[ERROR] Expected Measurements unavailable for :{x_t1_transformed}")
+            return z_t1_arr_expected    
+        
+        ang_arr = np.arange(-np.pi, np.pi, np.pi/180)
 
         theta_ = x_t1_transformed[2] 
 
         # Consider just till two decimals
-        laser_min = round(theta_ - 1.57, 2)
-        laser_max = round(theta_ + 1.57, 2)
+        laser_min = round(theta_ - np.pi/2, 2)
+        laser_max = round(theta_ + np.pi/2, 2)
         
         # Wrap laser_min and laser_max if out-of bounds
-        if laser_min < -3.14:
+        if laser_min < -np.pi:
             laser_min += 2 * np.pi  
-        if laser_max > 3.14:
+        if laser_max > np.pi:
             laser_max -= 2 * np.pi  
 
         # Basic Condition 
@@ -251,17 +279,114 @@ class SensorModel:
             # Adjust to ensure the total length is exactly 180 readings
             z_t1_arr_expected = np.concatenate((first_segment, second_segment))[:180]
 
+        return z_t1_arr_expected
 
+ 
+
+
+    def vec_expected_ray_measurements(self, x_t1_transformed_particles):
+        num_particles = x_t1_transformed_particles.shape[0]
+
+        # Convert to Occupancy-map coordinates
+        map_coords = np.floor(x_t1_transformed_particles[:, :2] / self.map_resolution).astype(int)
+        map_x = map_coords[:, 0]
+        map_y = map_coords[:, 1]
+
+        # Array of angles for ray tracing
+        ang_arr = np.arange(-np.pi, np.pi, np.pi / 180)
+
+        # Wrapping all the data. TODO: Find a better way 
+        particle_data = []
+
+        for i in range(num_particles):
+            map_x_i, map_y_i = map_x[i], map_y[i]
+            expected_ray_vals = self.particle_rays.get((map_x_i, map_y_i), np.zeros(180, dtype=np.float64))
+            particle_data.append((x_t1_transformed_particles[i], expected_ray_vals, ang_arr))
+
+        
+        with mp.Pool(processes=13) as pool:
+            z_t1_arr_expected_particles = pool.map(worker_function, particle_data)
+
+        return np.array(z_t1_arr_expected_particles)
+
+
+
+    def beam_range_finder_model_vectorized(self, z_t1_arr, x_t1_particles):
+        """
+        param[in] z_t1_arr : laser range readings [array of 180 values] at time t
+        param[in] x_t1_particles : array of particle states, shape [num_particles, 3] where each row is [x, y, theta] at time t [world_frame]
+        param[out] prob_zt1_arr : array of likelihoods for each particle, shape [num_particles]
+        """
+
+        num_particles = x_t1_particles.shape[0]
+
+        # Centre to Laser Transform 
+        x_t1_transformed_particles = self.vec_centre2laser_transform(x_t1_particles)
+
+        # Expected Ray Measurements 
+        z_t1_arr_expected_particles = self.vec_expected_ray_measurements(x_t1_transformed_particles)
+
+        ##### Vectorized Beam Model
+        p_hit_beams = np.zeros((num_particles, z_t1_arr.shape[0]), dtype=np.float64)
+        p_short_beams = np.zeros((num_particles, z_t1_arr.shape[0]), dtype=np.float64)
+        p_rand_beams = np.zeros((num_particles, z_t1_arr.shape[0]), dtype=np.float64)
+        p_max_beams = np.zeros((num_particles, z_t1_arr.shape[0]), dtype=np.float64)
+
+        # p_hit condition
+        valid_hit_idxs = (z_t1_arr >= 0) & (z_t1_arr <= self._max_range)
+        p_hit_beams[:, valid_hit_idxs] = np.exp(-np.square(z_t1_arr[valid_hit_idxs] - z_t1_arr_expected_particles[:, valid_hit_idxs]) / (2 * self._sigma_hit**2))
+
+        # p_rand condition
+        valid_rand_idxs = (z_t1_arr >= 0) & (z_t1_arr < self._max_range)
+        p_rand_beams[:, valid_rand_idxs] = 1 / self._max_range
+
+        # p_max condition (should vals outside max range be considered here?)
+        valid_max_idxs = (z_t1_arr == self._max_range)
+        p_max_beams[:, valid_max_idxs] = 1
+
+        # TODO: Vectorize p_short condition
+        for i in range(num_particles):
+            valid_short_idxs = (z_t1_arr >= 0) & (z_t1_arr <= z_t1_arr_expected_particles[i])
+            p_short_beams[i, valid_short_idxs] = self._lambda_short * np.exp(-self._lambda_short * z_t1_arr[valid_short_idxs])
+            
+        # Full-model probability 
+        p_beams = self._z_hit * p_hit_beams + self._z_short * p_short_beams + self._z_rand * p_rand_beams + self._z_max * p_max_beams
+
+        # Sum across beams for each particle
+        prob_zt1_arr = np.sum(p_beams, axis=1)
+
+        return prob_zt1_arr
+
+    def beam_range_finder_model(self, z_t1_arr, x_t1):
+        """
+        param[in] z_t1_arr : laser range readings [array of 180 values] at time t
+        param[in] x_t1 : particle state belief [x, y, theta] at time t [world_frame]
+        param[out] prob_zt1 : likelihood of a range scan zt1 at time t
+        """
+        """
+        TODO : Add your code here
+        """
+
+        # Centre to Laser Transform
+        x_t1_transformed = self.centre2laser_transform(x_t1, visualize=False)
+
+        # Expected Ray Measurments
+        z_t1_arr_expected = self.expected_ray_measurements(x_t1_transformed)
+
+        # Particle out-of Bound
+        if sum(z_t1_arr_expected) == 0:
+            return 0
+        
         scaling_coeff = 1.0
         
+        ##### Vectorized Beam Model
         p_hit_beams   = np.zeros(z_t1_arr.shape, dtype=np.float64)
         p_short_beams = np.zeros(z_t1_arr.shape, dtype=np.float64)
         p_rand_beams  = np.zeros(z_t1_arr.shape, dtype=np.float64)
         p_max_beams   = np.zeros(z_t1_arr.shape, dtype=np.float64)
         prob_zt1 = 1.0
         
-        ##### Vectorized Beam Model
-    
+        
         # p_hit condition
         valid_hit_idxs = (z_t1_arr >= 0) & (z_t1_arr <= self._max_range) 
         p_hit_beams[valid_hit_idxs] = np.exp(-( np.square(z_t1_arr[valid_hit_idxs] -z_t1_arr_expected[valid_hit_idxs]) / (2*np.square(self._sigma_hit)) ))
@@ -275,7 +400,7 @@ class SensorModel:
         p_rand_beams[valid_rand_idxs] = 1/self._max_range
 
         # p_max condition (float fcks up w/tout tolerance)
-        valid_max_idxs = np.isclose(z_t1_arr, z_t1_arr_expected, rtol=0.01)
+        valid_max_idxs = np.isclose(z_t1_arr, self._max_range, rtol=0.01)
         p_max_beams[valid_max_idxs] = 1
 
         
@@ -287,19 +412,15 @@ class SensorModel:
 
         # prob_zt1 = np.sum(p_beams)/p_beams.shape
         prob_zt1 = np.sum(p_beams)
-        # prob_zt1 = np.sum(np.log(p_beams))/p_beams.shape
-        # prob_zt1 = np.prod(p_beams)
         
 
-        
         # print(f"z_hit :{np.sum(self._z_hit*p_hit_beams)}")
         # print(f"z_short :{np.sum(self._z_short*p_short_beams)}")
         # print(f"z_rand :{np.sum(self._z_rand*p_rand_beams)}")
         # print(f"z_max :{np.sum(self._z_max*p_max_beams)}")
-        # if(prob_zt1 > 0.0):
-        #     print(f"Laser probs are :{prob_zt1}")
         # print("############################")
-        # # exit()
+        # exit()
         # For testing-purposes
         # prob_zt1 = 1.0
+
         return prob_zt1
