3b53470e99cdf48e0347de0050456f2049ae9c6a
shashwat <shashwatchawla@gmail.com>
2024-09-24 18:49:25 -0400

    [ADD] Vectorized motion-model

--- HW1/hw1_problem_set/problem_set/code/motion_model.py
+++ HW1/hw1_problem_set/problem_set/code/motion_model.py
@@ -19,21 +19,74 @@ class MotionModel:
         TODO : Tune Motion Model parameters here
         The original numbers are for reference but HAVE TO be tuned.
         """
-        # self._alpha1 = 0.01
-        # self._alpha2 = 0.01
-        # self._alpha3 = 0.01
-        # self._alpha4 = 0.01
+        self._alpha1 = 0.00001
+        self._alpha2 = 0.00001
+        self._alpha3 = 0.001
+        self._alpha4 = 0.001
 
 
-        self._alpha1 = 0.05
-        self._alpha2 = 0.05
-        self._alpha3 = 0.025
-        self._alpha4 = 0.025
+        # self._alpha1 = 0.05
+        # self._alpha2 = 0.05
+        # self._alpha3 = 0.025
+        # self._alpha4 = 0.025
 
-        self._alpha1 = 0.5
-        self._alpha2 = 0.5
-        self._alpha3 = 0.25
-        self._alpha4 = 0.25
+        # self._alpha1 = 0.5
+        # self._alpha2 = 0.5
+        # self._alpha3 = 0.25
+        # self._alpha4 = 0.25
+
+
+    def update_vectorized(self, u_t0, u_t1, X_t0):
+        """
+        param[in] u_t0 : particle state odometry reading [x, y, theta] at time (t-1) [odometry_frame]
+        param[in] u_t1 : particle state odometry reading [x, y, theta] at time t [odometry_frame]
+        param[in] X_t0 : particles state belief [p, x, y, theta] at time (t-1) [world_frame]
+        param[out] X_t1 :particles state belief [p, x, y, theta] at time t [world_frame]
+        """
+        ###################################################
+        #  Odom Motion Model-Probalistic Robotics(Ch5)  #  
+        ###################################################
+
+        # Delta-change in Odometry 
+        del_x, del_y, del_theta = u_t1 - u_t0
+        
+        del_rot_1 = np.arctan2(del_y, del_x) - u_t0[2]
+        del_trans = np.sqrt(np.square(del_x) + np.square(del_y))
+        del_rot_2 = del_theta - del_rot_1
+
+        del_rot_1_bar = del_rot_1 - np.random.normal(
+            loc=0.0, 
+            scale=np.sqrt(
+                self._alpha1*np.square(del_rot_1) + 
+                self._alpha2*np.square(del_trans) 
+                )
+            )
+        
+        del_trans_bar = del_trans - np.random.normal(
+            loc=0.0, 
+            scale=np.sqrt(
+                self._alpha3*np.square(del_trans) + 
+                self._alpha4*np.square(del_rot_1) +
+                self._alpha4*np.square(del_rot_2) 
+            )
+        )
+
+        del_rot_2_bar = del_rot_2 - np.random.normal(
+            loc=0.0,
+            scale=np.sqrt(
+                self._alpha1*np.square(del_rot_2) + 
+                self._alpha2*np.square(del_trans)
+            )
+        )        
+
+        X_t1 = np.zeros((X_t0.shape), dtype=np.float64)
+        # World-Frame Transformation
+        X_t1[:, 0] = X_t0[:, 0] + del_trans_bar*np.cos(X_t0[:, 2] + del_rot_1_bar)
+        X_t1[:, 1] = X_t0[:, 1] + del_trans_bar*np.sin(X_t0[:, 2] + del_rot_1_bar)
+        X_t1[:, 2] = X_t0[:, 2] + del_rot_1_bar + del_rot_2_bar
+
+        return X_t1 
+
 
 
     def update(self, u_t0, u_t1, x_t0):
